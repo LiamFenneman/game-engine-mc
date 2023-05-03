bb9d663399f5a31cec83b6d5be6d8dcb799d427c
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-03 15:22:11 +1200

    feat: basic setup for dev ui

---
+++ dev-ui/src/lib.rs
@@ -0,0 +1,57 @@
+#[derive(Default)]
+pub struct App {
+    show_window: bool,
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self { show_window } = self;
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        _frame.close();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Select a demo");
+
+            ui.separator();
+            if ui.button("Toggle Example").clicked() {
+                *show_window = !*show_window;
+            }
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label("powered by ");
+                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label(" and ");
+                    ui.hyperlink_to(
+                        "eframe",
+                        "https://github.com/emilk/egui/tree/master/crates/eframe",
+                    );
+                    ui.label(".");
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Game Engine Developer UI");
+            egui::warn_if_debug_build(ui);
+        });
+
+        if *show_window {
+            egui::Window::new("Window").show(ctx, |ui| {
+                ui.label("Windows can be moved by dragging them.");
+                ui.label("They are automatically sized based on contents.");
+                ui.label("You can turn on resizing and scrolling if you like.");
+                ui.label("You would normally choose either panels OR windows.");
+            });
+        }
+    }
+}
---
+++ dev-ui/src/main.rs
@@ -0,0 +1,16 @@
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    tracing_subscriber::fmt::init();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Show an image with eframe/egui",
+        options,
+        Box::new(|_cc| Box::<ge_dev_ui::App>::default()),
+    )
+}
