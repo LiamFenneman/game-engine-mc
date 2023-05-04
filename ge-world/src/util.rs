e0a2161b90366b381e135c1ee0ef393b918b7cb2
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-04 20:53:16 +1200

    feat: inverse lerp and remap utils

--- ge-world/src/util.rs
+++ ge-world/src/util.rs
@@ -14,9 +14,22 @@ pub const fn idx_to_pos(idx: usize, size: u32) -> Vector3<u32> {
     );
 }
 
+/// Linear interpolation.
 #[must_use]
 pub fn lerp(a: f64, b: f64, t: f64) -> f64 {
     return a * (1.0 - t) + b * t;
+}
+
+/// Inverse linear interpolation.
+#[must_use]
+pub fn inverse_lerp(a: f64, b: f64, t: f64) -> f64 {
+    return (t - a) / (b - a);
+}
+
+/// Remaps a value from one range to another.
+#[must_use]
+pub fn remap(t: f64, a_min: f64, a_max: f64, b_min: f64, b_max: f64) -> f64 {
+    return lerp(b_min, b_max, inverse_lerp(a_min, a_max, t));
 }
 
 #[must_use]
