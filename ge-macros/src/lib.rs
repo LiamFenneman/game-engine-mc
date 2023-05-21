e041e7cb55eccdfb33dec7baedd575a316486a85
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-21 18:31:30 +1200

    feat: add proc macro attr for debug timing

--- ge-macros/src/lib.rs
+++ ge-macros/src/lib.rs
@@ -4,6 +4,17 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::*;
 
+#[proc_macro_attribute]
+pub fn dbg_timer(_: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as proc_macro2::TokenStream);
+    quote! {
+        let dbg_time_start = std::time::Instant::now();
+        #input
+        tracing::debug!("elapsed: {:?}", dbg_time_start.elapsed());
+    }
+    .into()
+}
+
 #[proc_macro]
 pub fn dbg_time(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as proc_macro2::TokenStream);
